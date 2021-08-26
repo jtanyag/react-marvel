ea51162a8a64e1ba6683d9461a32fd7f73b4bfd5
jtanyag <jtanyag@gmail.com>
2021-08-26 14:18:16 -0700

    updates Header

--- src/components/Header/Header.styles.js
+++ src/components/Header/Header.styles.js
@@ -6,9 +6,26 @@ export const Wrapper = styled.div`
 `;
 
 export const Content = styled.div`
-  color: red;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  max-width: 1280px;
+  padding: 20px 0;
+  margin: 0 auto;
 `;
 
 export const MarvelLogoImg = styled.img`
-  height: 100px;
+  width: 200px;
+
+  @media screen and (max-width: 500px) {
+    width: 150px;
+  }
+`;
+
+export const MLogoImg = styled.img`
+  width: 50px;
+
+  @media screen and (max-width: 500px) {
+    width: 30px;
+  }
 `;
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,17 @@
-import { Wrapper, Content, MarvelLogoImg } from './Header.styles';
+import { Link } from 'react-router-dom';
 import MarvelLogo from '../../images/marvel_logo.svg';
+import MLogo from '../../images/m_logo.svg';
+import { Wrapper, Content, MarvelLogoImg, MLogoImg } from './Header.styles';
 
 const Header = () => (
   <Wrapper>
     <Content>
-      <MarvelLogoImg src={MarvelLogo} alt='Marvel-logo' />
+      <Link to='/'>
+        <MarvelLogoImg src={MarvelLogo} alt='Marvel-logo' />
+      </Link>
+      <Link to='https://developer.marvel.com/'>
+        <MLogoImg src={MLogo} alt='M-logo' />
+      </Link>
     </Content>
   </Wrapper>
 );
